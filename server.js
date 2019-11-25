99beb4134b69eb98b6ecd85c43ddacdf5e1d0ad3
pretrator <srivastavujjawal123@gmail.com>
2019-11-25 10:47:11 +0530

    Added dynamic port

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get("/api", function(request, response) {
     response.send("Chall raha hai bhosadike")
 });
 // listen for requests :)
-const listener = app.listen(80, function() {
+const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port ");
 });
